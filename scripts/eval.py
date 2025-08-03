ab9baa97da17d4d15b14c072a8f4e1268d6a7c8a
Marawan Gamal <marawan.gamal@live.com>
2025-08-03 13:55:58 -0400

    Chore: update configs

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,22 +1,53 @@
 import os
 import lm_eval
 from lm_eval import simple_evaluate
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
+from mtp.mthf.modelling_mthf import MultiTokenHF, MultiTokenHFConfig
+
+
+# Create model and tokenizer
+tok = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
+# model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-3B-Instruct") # hf model
+model = MultiTokenHF(
+    MultiTokenHFConfig(
+        model_name="meta-llama/Llama-3.2-3B-Instruct",
+        model_head="multihead",
+        horizon=2,
+        loss_type="joint",
+        pretrained=True,
+    )
+)  # mthf model
+
+# Move to GPU
 model.to("cuda")
+model.train()
+
+
+# Sample from model
+output = model.generate(
+    tok.encode("Hello, I'm a language model,", return_tensors="pt").to(model.device),
+    max_new_tokens=64,
+    do_sample=False,
+)
+print(tok.decode(output[0]))
 
 # Custom
 # script_dir = os.path.dirname(os.path.abspath(__file__))
 # task_manager = lm_eval.tasks.TaskManager(
 #     include_path=os.path.join(script_dir, "configs")
 # )
+
+# Evaluate
 output = simple_evaluate(
     model=lm_eval.models.huggingface.HFLM(pretrained=model),
     tasks=["gsm8k_cot"],
     batch_size=32,
-    cache_prompts=True,
-    device="cuda",
-    dtype="bfloat16",
+    limit=32,
 )
-print(output)
+
+for task_name in output["results"].keys():
+    for metric_name in output["results"][task_name].keys():
+        print(
+            f"{task_name}/{metric_name} = {output['results'][task_name][metric_name]}"
+        )
