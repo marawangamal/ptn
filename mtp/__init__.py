40a39119f519083977a9076672c3d5202bcc975c
Marawan Gamal <marawan.gamal@live.com>
2025-08-09 10:50:48 -0400

    Chore: add imports

--- mtp/__init__.py
+++ mtp/__init__.py
@@ -1,4 +1,1 @@
-from .mheads.multihead import Multihead
-from .mheads.stp import STP
 
-__all__ = ["Multihead", "STP"]
--- mtp/mheads/__init__.py
+++ mtp/mheads/__init__.py
@@ -1,5 +1,6 @@
 from mtp.mheads.multihead import Multihead
 from mtp.mheads.stp import STP
+from ._abc import AbstractDisributionHeadConfig, AbstractDisributionHeadOutput
 
 # NOTE: Should be in sync with mtp._types.ModelHeadType
 MHEADS = {"stp": STP, "multihead": Multihead, "none": lambda *args, **kwargs: None}
