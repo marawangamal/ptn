5f898bb50a60bf3ea6364d1a3838329ddcd0b61d
Marawan Gamal <marawan.gamal@live.com>
2025-08-20 14:13:46 -0400

    Add cp_condl to init

--- mtp/mheads/__init__.py
+++ mtp/mheads/__init__.py
@@ -1,8 +1,15 @@
 from mtp.mheads.cp_cond import CPCond
+from mtp.mheads.cp_condl import CPCondl
 from mtp.mheads.multihead import Multihead
 from mtp.mheads.stp import STP
 from mtp.mheads.cp import CP
 from ._abc import AbstractDisributionHeadConfig, AbstractDisributionHeadOutput
 
 # !! IMPORTANT !! ::  Keep in sync with mtp.mheads
-MHEADS = {"stp": STP, "multihead": Multihead, "cp": CP, "cp_cond": CPCond}
+MHEADS = {
+    "stp": STP,
+    "multihead": Multihead,
+    "cp": CP,
+    "cp_cond": CPCond,
+    "cp_condl": CPCondl,
+}
