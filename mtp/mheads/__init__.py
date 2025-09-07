5003db45f597003dc3ac1434faeb89439a44bb37
Marawan Gamal <marawan.gamal@live.com>
2025-09-07 18:00:58 -0400

    Add MoE without decoder

--- mtp/mheads/__init__.py
+++ mtp/mheads/__init__.py
@@ -4,6 +4,7 @@ from mtp.mheads.moe_decoder import MoED
 from mtp.mheads.mps import MPS
 from mtp.mheads.mps_decoder import MPSD
 from mtp.mheads.multihead import Multihead
+from mtp.mheads.moe import MoE
 from mtp.mheads.stp import STP
 from mtp.mheads.umps import UMPS
 from ._abc import AbstractDisributionHeadConfig, AbstractDisributionHeadOutput
@@ -14,6 +15,7 @@ MHEADS = {
     "multihead": Multihead,
     "cp": CP,
     "cp_decoder": CPD,
+    "moe": MoE,
     "moe_decoder": MoED,
     "mps": MPS,
     "mps_decoder": MPSD,
