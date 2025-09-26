f4795a25e57986d8fbb4627146c814bff369d71d
Marawan Gamal <marawan.gamal@live.com>
2025-09-26 19:07:18 -0400

    Update notebook

--- scripts/profile_instability.py
+++ scripts/profile_instability.py
@@ -1,49 +1,61 @@
 # Instability
+import argparse
 import torch
 from tqdm import tqdm
 from ctn.mheads import MHEADS
 from ctn.mheads._abc import AbstractDisributionHeadConfig
 import pandas as pd
 
-# Common HPs
-BATCH_SIZE = 8
-LR = 1e-3
-N_ITERS = 1000
-SEED = 0
 
-# Model hps
-H, R, Do = 100, 8, 2
+def main(horizons):
 
-models = [
-    {"name": r"LSF", "model": "mps", "iters": 0, "kwargs": {}},
-    {
-        "name": r"SGD",
-        "model": "mps",
-        "iters": 0,
-        "kwargs": {"use_scale_factors": False},
-    },
-]
+    # Common HPs
+    BATCH_SIZE = 8
+    N_ITERS = 100
+    LR = 1e-3
 
-for mdict in models:
-    model = MHEADS[mdict["model"]](
-        config=AbstractDisributionHeadConfig(
-            rank=R, d_model=1, d_output=Do, horizon=H, **mdict["kwargs"]
-        )
-    )
-    for i in tqdm(range(N_ITERS)):
-        x = torch.randn(BATCH_SIZE, 1)
-        y = torch.randint(0, Do, (BATCH_SIZE, H))
-        try:
-            if hasattr(model, "train_example"):
-                model.train_example(x, y)
-            else:
-                model(x, y)
-            mdict["iters"] += 1
-        except Exception as e:
-            break
+    # Model hps
+    R, Do = 2, 2
+
+    rows = []
+    for name, m, kwargs in zip(
+        ["LSF", "SGD"], ["mps", "mps"], [{}, {"use_scale_factors": False}]
+    ):
+        for H in horizons:
+            model = MHEADS[m](
+                config=AbstractDisributionHeadConfig(
+                    rank=R, d_model=1, d_output=Do, horizon=H, **kwargs
+                )
+            )
+            optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
+            for i in tqdm(range(N_ITERS)):
+                x = torch.randn(BATCH_SIZE, 1)
+                y = torch.randint(0, Do, (BATCH_SIZE, H))
+                try:
+                    if hasattr(model, "train_example"):
+                        model.train_example(x, y, lr=LR)
+                    else:
+                        output = model(x, y)
+                        loss = output.loss
+                        loss.backward()
+                        optimizer.step()
+                except Exception as e:
+                    break
+
+            rows.append(
+                {
+                    "name": name,
+                    "horizon": H,
+                    "iters": i + 1,
+                }
+            )
+
+    df = pd.DataFrame(rows)
+    print(df)
 
 
-df = pd.DataFrame(models)[["name", "iters"]]
-
-print("Max iterations: reached for each model")
-print(df)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--horizons", nargs="+", default=[100], type=int)
+    args = parser.parse_args()
+    main(**args.__dict__)
